dd92981ad1ac52fbc1947e6033d16cf6297e73b5
Ludy87 <Ludy87@users.noreply.github.com>
2023-04-24 11:35:26 +0200

    Update helper_object.py

--- src/pyecotrend_ista/helper_object.py
+++ src/pyecotrend_ista/helper_object.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Dict, List, Optional
 
 from dataclasses_json import DataClassJsonMixin, dataclass_json
