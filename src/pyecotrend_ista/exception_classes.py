f8f1ef40f40a745a652249c66a900aff1bc2b8ed
tr4nt0r <manni@zapto.de>
2024-06-18 17:13:44 +0200

    Enhance docstrings, add `ParserError` exception
    - Improved docstring for `ServerError` to clarify its purpose in handling server errors during requests.
    - Enhanced docstring for `LoginError` to specify its role in managing authentication-related errors.
    - Introduced `ParserError` exception class to handle errors encountered during parsing of request responses.
    - Added docstring to `ParserError` class to explain its purpose in managing parsing-related issues during request processing.

--- src/pyecotrend_ista/exception_classes.py
+++ src/pyecotrend_ista/exception_classes.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from typing import Any, TypeVar
+from typing import TypeVar
 import warnings
 
 T = TypeVar("T")
@@ -17,7 +17,8 @@ def deprecated(func: Callable[..., T], alias_func: str | None = None) -> Callabl
     func: Callable[..., T])
         The function to be marked as deprecated.
     alias_func : str, optional
-        The real function name to show as deprecated, in case the function was called through an alias.
+        The real function name to show as deprecated, in case the function was called
+        through an alias.
 
 
     Returns
@@ -49,29 +50,41 @@ class BaseError(Exception):
     """
 
 class ServerError(BaseError):
-    """Create ServerError."""
+    """Exception raised for server errors during requests.
 
-    def __init__(self) -> None:
-        """Initialize the object."""
-        super().__init__()
+    This exception is raised when a exception occurs during a request.
+    It inherits from BaseError  and can be used to handle server-related
+    issues specifically.
+    """
 
     def __str__(self) -> str:
-        """Returns a string representation of the error.."""
-        return "Server error, go to: https://ecotrend.ista.de/error"
-
+        """Return a string representation of the error.."""
+        return "Server error occurred during the request"
 
 
 class LoginError(BaseError):
-    """Create LoginError."""
+    """Exception raised for login- and authentication related errors.
 
-    def __init__(self, res: Any) -> None:
-        """Initialize the object with the result of the call."""
-        super().__init__(res)
-        self.res = res
+    This exception is raised when an authentication exception occurs during a request.
+    It inherits from BaseError and is used specifically to handle issues related
+    to authentication and login.
+    """
 
     def __str__(self) -> str:
-        """String representation of LoginFail."""
-        return f"Login fail, check your input! {self.res}"
+        """Return a string representation of an authentication error."""
+        return "An authentication error occurred during the request"
+
+class ParserError(BaseError):
+    """Exception raised for errors encountered during parsing.
+
+    This exception is raised when an error occurs during the parsing process
+    of the request response. It inherits from BaseError and can be used
+    to handle issues specifically related to parsing.
+    """
+
+    def __str__(self) -> str:
+        """Return a string representation of parser error."""
+        return "Error occurred during parsing of the request response"
 
 
 # The MIT License (MIT)
