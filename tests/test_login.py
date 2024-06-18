50dc9ece275fd3b0f846cc64f74b91a5418ff553
Mr. Bubbles <manni@zapto.de>
2024-06-18 15:14:31 +0200

    Enhance test coverage and update test dependencies (#178)
    * Enhance test coverage and update test dependencies
    
    - Added syrupy==4.6.1 to requirements_test.txt for snapshot testing.
    - Enhanced conftest.py:
      - Added import for RequestsMock.
      - Updated ista_client fixture to use a default email if not provided.
      - Updated mock_requests_login fixture to include return type annotation and return statement.
    - Improved test_login.py:
      - Added imports for MagicMock, RequestsMock, JSONDecodeError, and SnapshotAssertion.
      - Added snapshot assertion in test_demo_user_login.
      - Added tests for handling exceptions in demo_user_login method.
      - Added test for JSONDecodeError in demo_user_login method.
    - Added test_set_account.py:
      - Added tests for the _set_account method.
      - Included tests for handling exceptions and JSONDecodeError.
    
    * Add unit tests, update .gitignore and requirements
    
    This commit introduces the following changes:
    
    - New Unit Tests: Added unit tests for the `get_raw` and `consum_raw` methods in `test_consumptions.py`.
    - Updated .gitignore: Updated the .gitignore file to exclude unit tests and unit test related data files (`tests/*.py`, `tests/*.ambr`, `!tests/*.json`)
    Added Dependency: Added pytest-dataset dependency to requirements_test.txt for unit test data fixtures.

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,14 @@
 """Tests for Login methods."""
 
+from unittest.mock import MagicMock
+
 import pytest
-from pyecotrend_ista import PyEcotrendIsta
+import requests
+from pyecotrend_ista import PyEcotrendIsta, ServerError
+from pyecotrend_ista.const import API_BASE_URL
+from requests.exceptions import JSONDecodeError
+from requests_mock.mocker import Mocker as RequestsMock
+from syrupy.assertion import SnapshotAssertion
 
 from tests.conftest import DEMO_EMAIL, TEST_EMAIL
 
@@ -13,12 +20,36 @@ def test_login(ista_client: PyEcotrendIsta) -> None:
     assert ista_client.login() == "ACCESS_TOKEN"
 
 
-@pytest.mark.parametrize("ista_client", [TEST_EMAIL, DEMO_EMAIL], indirect=True)
+@pytest.mark.parametrize("ista_client", [DEMO_EMAIL], indirect=True)
 @pytest.mark.usefixtures("mock_requests_login")
-def test_account_data(ista_client: PyEcotrendIsta) -> None:
+def test_demo_user_login(ista_client: PyEcotrendIsta, snapshot: SnapshotAssertion) -> None:
     """Test Login method."""
-    ista_client.login()
 
-    assert ista_client._account["activeConsumptionUnit"] == "7a226e08-2a90-4db9-ae9b-8148901c6ec2"
-    assert ista_client._uuid == "7a226e08-2a90-4db9-ae9b-8148901c6ec2"
-    assert ista_client.get_uuids() == ["7a226e08-2a90-4db9-ae9b-8148901c6ec2"]
+    assert ista_client.demo_user_login() == snapshot
+
+
+@pytest.mark.parametrize(
+    ("exception", "expected_exception"), ([(requests.RequestException, ServerError), (requests.Timeout, ServerError)])
+)
+def test_demo_user_login_exceptions(
+    ista_client: PyEcotrendIsta,
+    requests_mock: RequestsMock,
+    exception,
+    expected_exception,
+) -> None:
+    """Test exceptions for method `demo_user_login`."""
+
+    requests_mock.get(f"{API_BASE_URL}demo-user-token", exc=exception)
+
+    with pytest.raises(expected_exception=expected_exception):
+        ista_client.demo_user_login()
+
+
+def test_demo_user_login_parser_exception(ista_client: PyEcotrendIsta, requests_mock: RequestsMock) -> None:
+    """Test JSONDecodeError exception for method `demo_user_login`."""
+
+    json_encoder = MagicMock().json.side_effect = JSONDecodeError("test", "test", 0)
+    requests_mock.get(f"{API_BASE_URL}demo-user-token", json_encoder=json_encoder)
+
+    with pytest.raises(expected_exception=ServerError):
+        ista_client.demo_user_login()
