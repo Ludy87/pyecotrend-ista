2a73529953535fce917a67020778e46a6c310c43
Ludy87 <Ludy87@users.noreply.github.com>
2024-07-22 20:07:21 +0200

    Update exception_classes.py

--- src/pyecotrend_ista/exception_classes.py
+++ src/pyecotrend_ista/exception_classes.py
@@ -49,7 +49,8 @@ def deprecated(func: Callable[..., T], alias_func: str | None = None) -> Callabl
                 f"The `{alias_func}` function is deprecated and will be removed in a future release. "
                 f"Use `{func.__name__}` instead."
             )
-        warning_message = f"The `{func.__name__}` function is deprecated and will be removed in a future release."
+        else:
+            warning_message = f"The `{func.__name__}` function is deprecated and will be removed in a future release."
 
         warnings.warn(warning_message, category=DeprecationWarning, stacklevel=2)
         return func(*args, **kwargs)
